4232bdf58e3c43ae4e192976bff479184fa52b1a
johnnywiller <johnnywiller10@gmail.com>
2017-12-14 17:51:36 -0200

    improve client session

--- ChatClient/src/br/furb/dss/ListeningServer.java
+++ ChatClient/src/br/furb/dss/ListeningServer.java
@@ -1,5 +1,37 @@
 package br.furb.dss;
 
-public class ListeningServer {
+import java.io.IOException;
 
+public class ListeningServer extends Thread {
+
+	private ServerSocket server;
+	private final int MAX_BUF = 512;
+	
+	public ListeningServer(ServerSocket server) {
+		this.server = server;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+	
+	private void listen() throws IOException {
+		
+		byte[] receivedPacket;
+		
+		while(true) {
+			
+			receivedPacket = new byte[MAX_BUF];
+			
+			server.getIn().read(receivedPacket);
+			
+			byte[]
+			
+		}
+		
+	}
+	
+	
+	
 }
